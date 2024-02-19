252bd843f95b60a87ae24aec140406484b70c287
Ken Wu <kenwu1009us@gmail.com>
2024-02-19 15:51:06 -0800

    add sql scripts

---
+++ sql_scripts/01_create_tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE event (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    date_time DATETIME,
+    tickets_total INT,
+    tickets_available INT
+);
+
+
+CREATE TABLE user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+
+CREATE TABLE reservation (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    tickets_reserved INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    FOREIGN KEY (event_id) REFERENCES event(id)
+);
---
+++ sql_scripts/02_insert_initial_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO event (name, description, date_time, tickets_total, tickets_available)
+    VALUES
+    ('National Awareness Day', 'You won\'t have time for sleeping, soldier, not with all the bed making you\'ll be doing. Guess again. No, I\'m Santa Claus! Bender! Ship! Stop bickering or I\'m going to come back there and change your opinions manually!', '2027-03-05 13:00:00', 10, 10),
+    ('Universal Entrepreneurship Expo', 'I suppose I could part with \'one\' and still be feared... Enough about your promiscuous mother, Hermes! We have bigger problems. Ummm...to eBay? Can I use the gun?', '2013-02-21 15:00:00', 5, 5),
+    ('Wine festival', 'All I want is to be a monkey of moderate intelligence who wears a suit... that\'s why I\'m transferring to business school! Meh. We\'ll go deliver this crate like professionals, and then we\'ll go home.', '2024-12-11 14:00:00', 1, 1),
+    ('Annual Bicycle Appreciation Day', 'Yes, if you make it look like an electrical fire. When you do things right, people won\'t be sure you\'ve done anything at all. Oh dear! She\'s stuck in an infinite loop, and he\'s an idiot! Well, that\'s love for you.', '2007-03-01 13:00:00', 200, 200),
+    ('Rocket to Mars', 'I\'m nobody\'s taxi service; I\'m not gonna be there to catch you every time you feel like jumping out of a spaceship. I\'m the Doctor, I\'m worse than everyone\'s aunt. *catches himself* And that is not how I\'m introducing myself.', '2047-10-21 09:00:00', 0, 0);
