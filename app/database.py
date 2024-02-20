a71db0faddae49e92fc690d7fb35f461d670006c
Ken Wu <kenwu1009us@gmail.com>
2024-02-20 11:18:28 -0800

    async db

--- app/database.py
+++ app/database.py
@@ -1,9 +1,24 @@
-from sqlmodel import SQLModel, create_engine
+# from sqlmodel import SQLModel, create_engine
+# from config.settings import settings
+
+# DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
+# engine = create_engine(DATABASE_URL, echo=True)
+
+
+# def init_db():
+#     SQLModel.metadata.create_all(engine)
+
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 from config.settings import settings
 
-DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
-engine = create_engine(DATABASE_URL, echo=True)
+DATABASE_URL = f"mysql+aiomysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
