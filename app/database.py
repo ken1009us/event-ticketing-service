3bedae715e75731b7127103b24f3315715340a33
Ken Wu <kenwu1009us@gmail.com>
2024-02-20 11:44:44 -0800

    remove comment out code for db script

--- app/database.py
+++ app/database.py
@@ -1,13 +1,3 @@
-# from sqlmodel import SQLModel, create_engine
-# from config.settings import settings
-
-# DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}/{settings.DB_NAME}"
-# engine = create_engine(DATABASE_URL, echo=True)
-
-
-# def init_db():
-#     SQLModel.metadata.create_all(engine)
-
 from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
